83c87f54d90abd698fec2dcc7dae022142abc0b2
Yogesh <yogeshkhande15@gmail.com>
2022-10-24 14:57:13 +0530

    First Commit

---
+++ src/Basic/Test1.java
@@ -0,0 +1,17 @@
+package Basic;
+
+public class Test1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Main method running");
+		m1();
+		
+	}
+	
+	public static void m1()
+	{
+		System.out.println("HI");
+	}
+
+}
